79436d697e9efa6ce9c47de27db1117d67d3806b
pang dev <pangdev58@gmail.com>
2021-02-12 18:48:30 +0700

    new: add newsfeed router

--- httpd/main.go
+++ httpd/main.go
@@ -3,12 +3,16 @@ package main
 import (
 	"github.com/gin-gonic/gin"
 	handler "github.com/jaikox2/rest-api-gin/httpd/handler/ping"
+	"github.com/jaikox2/rest-api-gin/platform/newsfeed"
 )
 
 func main() {
+	feed := newsfeed.New()
 	r := gin.Default()
 
-	r.GET("/ping", handler.PingGet("responese a string"))
+	r.GET("/ping", handler.PingGet("Test pong"))
+	r.GET("/newsfeed", handler.NewsFeedGet(feed))
+	r.POST("/newsfeed", handler.NewsFeedPost(feed))
 
 	r.Run(":8080")
 }
